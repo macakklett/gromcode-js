5472ab52a320aa4d4066c43f036282df38bee9bc
macakklett <macanvitalijj@gmail.com>
2024-06-24 22:47:33 +0300

    fix

--- 21_Dom/02_find_element/index.js
+++ 21_Dom/02_find_element/index.js
@@ -1,10 +1,10 @@
-const getTitleElement = () => {
+export const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 };
 
-const getInputElement = () => {
+export const getInputElement = () => {
   const inputElem = document.querySelector('input[type="text"]');
   console.dir(inputElem);
   return inputElem;
