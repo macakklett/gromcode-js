0b8e1defe4849101bc3408b11b5a68c21625f6bf
macakklett <macanvitalijj@gmail.com>
2024-06-28 21:06:17 +0300

    ls24-4

---
+++ 24_Date/04_Students_birthday/index.js
@@ -0,0 +1,25 @@
+const students = [
+  { name: 'Tom', birthDate: '01/15/2010' },
+  { name: 'Ben', birthDate: '01/17/2008' },
+  { name: 'Sam', birthDate: '03/15/2010' },
+];
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const studentsBirthdays = students => {
+  return students.reduce((acc, { name, birthDate }) => {
+    // const [month] = birthDate.split('/').map(str => Math.floor(Number(str)));
+    // const monthText = months[month - 1];
+    const month = new Date(birthDate).getMonth();
+    const monthText = months[month];
+
+    if (!acc[monthText]) {
+      acc[monthText] = [];
+    }
+    acc[monthText].push(name);
+
+    return acc;
+  }, {});
+};
+
+console.log(studentsBirthdays(students));
